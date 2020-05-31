3dd06b46ee52066bcfda7201a582429ece9a91d4
Alessandro Molari <molari.alessandro@gmail.com>
2020-05-31 13:07:16 +0200

    Add magic mouse cursor

--- src/data/colorscheme.js
+++ src/data/colorscheme.js
@@ -1,66 +1,9 @@
-const grey = {
-  platinum: "#E9E9E9",
-  gray: "#BEBEBE",
-  quickSilver: "#A8A8A8",
-  blackCoral: "#5B5D66",
-  onyx: "#3C3C3C",
-  raisinBlack: "#262626",
-  eerieBlack: "#1E1E1E",
-  smokyBlack: "#0F0F0F",
-};
-
-const blueGrey = {
-  cadetBlue: "#9CB2CB",
-  shadowBlue: "#7B91B4",
-  blueYonder: "#627497",
-};
-
-const blue = {
-  blueJeans: "#4EACED",
-  bleuDeFrance: "#1E88E5",
-  denim: "#1561B8",
-};
-
-const brown = {
-  cinereous: "#A88D88",
-  burnishedBrown: "#92726D",
-  liver: "#6E504C",
-};
-
-const yellow = {
-  yellowCraiola: "#FCEB8B",
-  jasmine: "#FADF7F",
-  camel: "#B99C59",
-};
-
-const orange = {
-  copper: "#E8906B",
-  burntSienna: "#DE6E4B",
-  mediumCarmine: "#AE4D35",
-};
-
-const red = {
-  englishLavender: "#BD7B97",
-  chinaRose: "#A14E6B",
-  quinacridoneMagenta: "#7F374C",
-};
-
-const green = {
-  pistachio: "#AFCD84",
-  olivine: "#97B86D",
-  mossGreen: "#799657",
-};
-
-const violet = {
-  glossyGrape: "#AF90AF",
-  chineseViolet: "#8D668C",
-  englishViolet: "#6B476A",
+// XXX: Keep it in sync with scss colorscheme.
+const colorscheme = {
+  primary: "#1e88e5", // $blue
+  defaultBackground: "#262626", // $body-bg
 };
 
 module.exports = {
-  colors: { grey, blueGrey, blue, brown, yellow, orange, red, green, violet },
-
-  theme: {
-    defaultBackground: grey.raisinBlack,
-  },
+  colorscheme: colorscheme,
 };
--- src/scripts/init.js
+++ src/scripts/init.js
@@ -1,1 +1,12 @@
 import "bootstrap";
+import $ from "jquery";
+import barba from "@barba/core";
+import LocomotiveScroll from "locomotive-scroll";
+import { mouseCursor } from "../components/magic-mouse-cursor/magic-mouse-cursor";
+
+// barba.init({});
+// const scroll = new LocomotiveScroll();
+
+$(document).ready(() => {
+  mouseCursor();
+});
